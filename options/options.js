63c26c1251e8840d6b8f098fb1086623891c4b9a
Crowdbotics <team@crowdbotics.com>
2024-03-22 20:50:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pratik-khadloya-47252",
+  url: "https://pratik-khadloya-47252.botics.co",
+  api: "https://pratik-khadloya-47252.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
